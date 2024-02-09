5e8c4ffb701f6dc608f875226b962de412d8dc6a
Ahmad Abdullah <135378526+Ahmad1137@users.noreply.github.com>
2024-02-09 13:08:55 +0500

    background changer

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+function App() {
+  const [color, setColor] = useState("red")
+
+  return (
+    <div
+      className="w-full h-screen duration-200"
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-3 rounded-3xl">
+          <button
+            className="outline-none px-4 py-1 rounded-lg text-white"
+            style={{ backgroundColor: "red" }}
+            onClick={() => setColor("red")}
+          >
+            Red
+          </button>
+          <button
+            className="outline-none px-4 py-1 rounded-lg text-white"
+            style={{ backgroundColor: "green" }}
+            onClick={() => setColor("green")}
+          >
+            Green
+          </button>
+          <button
+            className="outline-none px-4 py-1 rounded-lg text-white"
+            style={{ backgroundColor: "blue" }}
+            onClick={() => setColor("blue")}
+          >
+            Blue
+          </button>
+          <button
+            className="outline-none px-4 py-1 rounded-lg text-white"
+            style={{ backgroundColor: "olive" }}
+            onClick={() => setColor("olive")}
+          >
+            Olive
+          </button>
+          <button
+            className="outline-none px-4 py-1 rounded-lg text-white"
+            style={{ backgroundColor: "Black" }}
+            onClick={() => setColor("black")}
+          >
+            Black
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
